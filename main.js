b91ee23d7bf6981136d677ddf74e258f87b8a450
CC Webb <thewebbdesigns@gmail.com>
2021-01-12 00:06:41 -0700

    testUpload

---
+++ main.js
@@ -0,0 +1,14 @@
+const links = [
+    {label: "Week 1", url: "week1/index.html"},
+    {label: "Week 2", url: "week2/week2.html"},
+    {label: "Week 3", url: "week3/week3.html"},   
+    {label: "Week 4", url: "week4/week4.html"},
+    {label: "Week 5", url: "week5/week05.html"},
+    {label: "Week 6", url: "week6/wwek6.html"},
+    {label: "Week 7", url: "week7/week7.html"},
+    {label: "Week 8", url: "week8/week8.html"},
+    {label: "Week 9", url: "week9/week9.html"},
+    {label: "Week 10", url: "week10/week10.html"},
+    {label: "Week 11", url: "week11/week11.html"},
+    {label: "Week 12", url: "week12/week12.html"},
+]
