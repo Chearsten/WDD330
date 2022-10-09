0041353c58b5d3e2afe6aa73dbfcc29563f11657
CC Webb <thewebbdesigns@gmail.com>
2022-10-09 00:52:05 -0600

    New content

--- js/week03.js
+++ js/week03.js
@@ -108,34 +108,61 @@ console.table (inventors);
   }
 
   // Array.prototype.reduce()
-  // 4. How many years did all the inventors live?
+   // 4. How many years did all the inventors live?
+
+   const reduced = inventors.reduce((total, inventor) => total + (inventor.passed - inventor.year), 0);
+   console.log(reduced);
+
 
   // 5. Sort the inventors by years lived
 
+  const oldest = inventors.sort(function(a, b) {
+    const lastInventor = a.passed - a.year;
+    const nextInventor = b.passed - b.year;
+    return lastInventor > nextInventor ? -1 : 1;});
+    console.table(oldest);
+
   // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-  // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-  // goto the link above and open the console. Paste the following two lines in.  That will create a list of links in memory that you can reference through the console. Use that list to finish the problem.
-  // const category = document.querySelector('.mw-category');
-  // const links = Array.from(category.querySelectorAll('a'));
+      // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+      // goto the link above and open the console. Paste the following two lines in.  That will create a list of links in memory that you can reference through the console. Use that list to finish the problem.
+      // const category = document.querySelector('.mw-category');
+      // const links = Array.from(category.querySelectorAll('a'));
 
-  // 7. sort Exercise
-  // Sort the people alphabetically by last name
+      // 7. sort Exercise
+      // Sort the people alphabetically by last name
 
-  // 8. Reduce Exercise
-  // Sum up the instances of each of these
-  const data = [
-    'car',
-    'car',
-    'truck',
-    'truck',
-    'bike',
-    'walk',
-    'car',
-    'van',
-    'bike',
-    'walk',
-    'car',
-    'van',
-    'car',
-    'truck'
-  ];
+      const alpha = people.sort((lastOne, nextOne) => {
+        const [aLast, aFirst] = lastOne.split(', ');
+        const [bLast, bFirst] = nextOne.split(', ');
+        return aLast > bLast ? 1 : -1;});
+        console.log(alpha);
+  
+  
+        // 8. Reduce Exercise
+        // Sum up the instances of each of these
+        const data = [
+          'car',
+          'car',
+          'truck',
+          'truck',
+          'bike',
+          'walk',
+          'car',
+          'van',
+          'bike',
+          'walk',
+          'car',
+          'van',
+          'car',
+          'truck'
+        ];
+  
+        const transportation = data.reduce(function(obj, item) {
+        if (!obj[item]) {
+          obj[item] = 0;
+        }
+        obj[item]++;
+        return obj;
+      }, {});
+  
+      console.log(transportation);
