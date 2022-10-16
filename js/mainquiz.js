3486eddc64ed887d05fdf7b722a689a6d9ca89b1
CC Webb <thewebbdesigns@gmail.com>
2022-10-15 23:20:41 -0600

    Update mainquiz.js

--- js/mainquiz.js
+++ js/mainquiz.js
@@ -12,6 +12,44 @@ const quiz = [
     ["What is Batman's real name?","Bruce Wayne"]
 ];
 
+function start(quiz){
+    let score = 0;
+
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
+        alert('Correct!');
+        score++;
+        } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+    }
+}
+start(quiz);
+
+/*const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
 let score = 0 // initialize score
 
 for(const [question,answer] of quiz){
@@ -25,5 +63,5 @@ for(const [question,answer] of quiz){
 }
 
 // At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);*/
 
